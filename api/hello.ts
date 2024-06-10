b995f6adc7410ff504fe477cd4bd70b795a2a6be
Christopher Cowan <soundcommchris@gmail.com>
2024-06-10 11:58:30 -0700

    test1

--- api/hello.ts
+++ api/hello.ts
@@ -1,11 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
-// export default function (request: VercelRequest, response: VercelResponse){
-//     response.status(200).send({
-//         message: 'Hello World!'
-//     })
-// }
-
-export function GET(request: Request) {
-    return new Response(`Hello from the serverless function`);
-  }
+export default function (request: VercelRequest, response: VercelResponse){
+    response.status(200).send({
+        message: 'Hello World!'
+    })
+}
